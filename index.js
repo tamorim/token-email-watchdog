6f5305959d5443898371b8b0337443e982ab405e
Thor Amorim <thoramorim@gmail.com>
2016-03-27 21:45:52 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const Imap = require('imap')
+const ncp = require('copy-paste')
+const notifier = require('node-notifier')
+
+const imap = new Imap({
+  user: '',
+  password: '',
+  host: '',
+  port: '',
+  tls: true
+})
+
+const openInbox = () => {
+  imap.openBox('INBOX', err => {
+    if (err) throw err
+  })
+}
+
+const copyToken = () => {
+  imap.search(['UNSEEN', ['HEADER', 'FROM', '']], (err, data) => {
+    if (err) throw err
+
+    if (data.length === 0) return;
+
+    const mail = data.pop();
+    const f = imap.fetch(mail, { bodies: 'HEADER.FIELDS (SUBJECT)' })
+
+    f.on('message', msg => {
+      msg.on('body', stream => {
+        let buffer = ''
+        stream.on('data', chunk => buffer += chunk.toString())
+
+        stream.once('end', () => {
+          const token = buffer.match(/\d{6}/)[0]
+
+          ncp.copy(token, () => {
+            notifier.notify({
+              title: 'Token E-mail Watchdog',
+              message: `Copied token ${token} on the clipboard!`
+            })
+
+            imap.move(mail, '[Gmail]/Todos os e-mails', err => {
+              if (err) throw err
+            })
+          })
+        })
+      })
+    })
+
+    f.once('error', err => console.log('Fetch error: ' + err))
+  })
+}
+
+imap.once('ready', openInbox)
+imap.on('mail', copyToken)
+imap.once('error', err => console.log(err))
+imap.once('end', () => process.exit())
+imap.connect()
+
